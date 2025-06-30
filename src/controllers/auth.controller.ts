f64795f001ae4ef30a1fb6e0994a2eaa0bd32c65
dianck2002 <dianck2002@gmail.com>
2025-06-30 06:12:41 +0000

    first build

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import prisma from "../../prisma";
+import prisma from "../prisma";
 import { genSalt, hash } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import Handlebars from "handlebars";
--- src/prisma.ts
+++ src/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "../generated/prisma";
 
 
 export default new PrismaClient({log: ["query", "info", "warn", "error"]});
